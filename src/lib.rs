e1029ddd96c19796964f63940127835243192064
Tamme Schichler <tamme@schichler.dev>
2020-09-01 08:51:36 +0200

    Added missing documentation examples

--- src/lib.rs
+++ src/lib.rs
@@ -38,6 +38,16 @@ use core::fmt::{
 /// Implements [`Debug`] by calling the stored closure.
 ///
 /// [`Debug`]: https://doc.rust-lang.org/stable/core/fmt/trait.Debug.html
+///
+/// # Example
+///
+/// ```rust
+/// use fn_formats::DebugFmt;
+///
+/// let debug = DebugFmt(|f| write!(f, "debug"));
+///
+/// assert_eq!(format!("{:?}", debug), "debug");
+/// ```
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Default)]
 pub struct DebugFmt<Fmt>(pub Fmt)
 where
@@ -49,6 +59,9 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> Debug for DebugFmt<Fmt> {
 	}
 }
 
+/// ```rust
+/// let _: fn_formats::DebugFmt<_> = (|f: &mut core::fmt::Formatter| Ok(())).into();
+/// ```
 impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for DebugFmt<Fmt> {
 	fn from(fmt: Fmt) -> Self {
 		Self(fmt)
@@ -58,6 +71,16 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for DebugFmt<Fmt> {
 /// Implements [`Display`] by calling the stored closure.
 ///
 /// [`Display`]: https://doc.rust-lang.org/stable/core/fmt/trait.Display.html
+///
+/// # Example
+///
+/// ```rust
+/// use fn_formats::DisplayFmt;
+///
+/// let display = DisplayFmt(|f| write!(f, "display"));
+///
+/// assert_eq!(format!("{}", display), "display");
+/// ```
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Default)]
 pub struct DisplayFmt<Fmt>(pub Fmt)
 where
@@ -69,6 +92,9 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> Display for DisplayFmt<Fmt> {
 	}
 }
 
+/// ```rust
+/// let _: fn_formats::DisplayFmt<_> = (|f: &mut core::fmt::Formatter| Ok(())).into();
+/// ```
 impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for DisplayFmt<Fmt> {
 	fn from(fmt: Fmt) -> Self {
 		Self(fmt)
@@ -79,6 +105,17 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for DisplayFmt<Fmt> {
 ///
 /// [`Debug`]: https://doc.rust-lang.org/stable/core/fmt/trait.Debug.html
 /// [`Display`]: https://doc.rust-lang.org/stable/core/fmt/trait.Display.html
+///
+/// # Example
+///
+/// ```rust
+/// use fn_formats::DebugDisplayFmt;
+///
+/// let debug_display = DebugDisplayFmt(|f| write!(f, "debug or display"));
+///
+/// assert_eq!(format!("{:?}", debug_display), "debug or display");
+/// assert_eq!(format!("{}", debug_display), "debug or display");
+/// ```
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Default)]
 pub struct DebugDisplayFmt<Fmt>(pub Fmt)
 where
@@ -96,6 +133,9 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> Display for DebugDisplayFmt<Fmt> {
 	}
 }
 
+/// ```rust
+/// let _: fn_formats::DebugDisplayFmt<_> = (|f: &mut core::fmt::Formatter| Ok(())).into();
+/// ```
 impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for DebugDisplayFmt<Fmt> {
 	fn from(fmt: Fmt) -> Self {
 		Self(fmt)
@@ -106,6 +146,20 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for DebugDisplayFmt<Fmt> 
 ///
 /// [`Debug`]: https://doc.rust-lang.org/stable/core/fmt/trait.Debug.html
 /// [`Display`]: https://doc.rust-lang.org/stable/core/fmt/trait.Display.html
+///
+/// # Example
+///
+/// ```rust
+/// use fn_formats::DebugDisplayFmtSeparate;
+///
+/// let debug_display = DebugDisplayFmtSeparate {
+///     debug: |f| write!(f, "debug"),
+///     display: |f| write!(f, "display"),
+/// };
+///
+/// assert_eq!(format!("{:?}", debug_display), "debug");
+/// assert_eq!(format!("{}", debug_display), "display");
+/// ```
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Default)]
 pub struct DebugDisplayFmtSeparate<DebugFmt, DisplayFmt>
 where
@@ -139,6 +193,16 @@ impl<
 /// Implements [`Binary`] by calling the stored closure.
 ///
 /// [`Binary`]: https://doc.rust-lang.org/stable/core/fmt/trait.Binary.html
+///
+/// # Example
+///
+/// ```rust
+/// use fn_formats::BinaryFmt;
+///
+/// let binary = BinaryFmt(|f| write!(f, "binary"));
+///
+/// assert_eq!(format!("{:b}", binary), "binary");
+/// ```
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Default)]
 pub struct BinaryFmt<Fmt>(pub Fmt)
 where
@@ -150,6 +214,9 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> Binary for BinaryFmt<Fmt> {
 	}
 }
 
+/// ```rust
+/// let _: fn_formats::BinaryFmt<_> = (|f: &mut core::fmt::Formatter| Ok(())).into();
+/// ```
 impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for BinaryFmt<Fmt> {
 	fn from(fmt: Fmt) -> Self {
 		Self(fmt)
@@ -159,6 +226,16 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for BinaryFmt<Fmt> {
 /// Implements [`LowerExp`] by calling the stored closure.
 ///
 /// [`LowerExp`]: https://doc.rust-lang.org/stable/core/fmt/trait.LowerExp.html
+///
+/// # Example
+///
+/// ```rust
+/// use fn_formats::LowerExpFmt;
+///
+/// let lower_exp = LowerExpFmt(|f| write!(f, "lower exp"));
+///
+/// assert_eq!(format!("{:e}", lower_exp), "lower exp");
+/// ```
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Default)]
 pub struct LowerExpFmt<Fmt>(pub Fmt)
 where
@@ -170,6 +247,9 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> LowerExp for LowerExpFmt<Fmt> {
 	}
 }
 
+/// ```rust
+/// let _: fn_formats::LowerExpFmt<_> = (|f: &mut core::fmt::Formatter| Ok(())).into();
+/// ```
 impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for LowerExpFmt<Fmt> {
 	fn from(fmt: Fmt) -> Self {
 		Self(fmt)
@@ -179,6 +259,16 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for LowerExpFmt<Fmt> {
 /// Implements [`LowerHex`] by calling the stored closure.
 ///
 /// [`LowerHex`]: https://doc.rust-lang.org/stable/core/fmt/trait.LowerHex.html
+///
+/// # Example
+///
+/// ```rust
+/// use fn_formats::LowerHexFmt;
+///
+/// let lower_hex = LowerHexFmt(|f| write!(f, "lower hex"));
+///
+/// assert_eq!(format!("{:x}", lower_hex), "lower hex");
+/// ```
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Default)]
 pub struct LowerHexFmt<Fmt>(pub Fmt)
 where
@@ -190,6 +280,9 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> LowerHex for LowerHexFmt<Fmt> {
 	}
 }
 
+/// ```rust
+/// let _: fn_formats::LowerHexFmt<_> = (|f: &mut core::fmt::Formatter| Ok(())).into();
+/// ```
 impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for LowerHexFmt<Fmt> {
 	fn from(fmt: Fmt) -> Self {
 		Self(fmt)
@@ -199,6 +292,16 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for LowerHexFmt<Fmt> {
 /// Implements [`Octal`] by calling the stored closure.
 ///
 /// [`Octal`]: https://doc.rust-lang.org/stable/core/fmt/trait.Octal.html
+///
+/// # Example
+///
+/// ```rust
+/// use fn_formats::OctalFmt;
+///
+/// let octal = OctalFmt(|f| write!(f, "octal"));
+///
+/// assert_eq!(format!("{:o}", octal), "octal");
+/// ```
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Default)]
 pub struct OctalFmt<Fmt>(pub Fmt)
 where
@@ -210,6 +313,9 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> Octal for OctalFmt<Fmt> {
 	}
 }
 
+/// ```rust
+/// let _: fn_formats::OctalFmt<_> = (|f: &mut core::fmt::Formatter| Ok(())).into();
+/// ```
 impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for OctalFmt<Fmt> {
 	fn from(fmt: Fmt) -> Self {
 		Self(fmt)
@@ -219,6 +325,16 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for OctalFmt<Fmt> {
 /// Implements [`Pointer`] by calling the stored closure.
 ///
 /// [`Pointer`]: https://doc.rust-lang.org/stable/core/fmt/trait.Pointer.html
+///
+/// # Example
+///
+/// ```rust
+/// use fn_formats::PointerFmt;
+///
+/// let pointer = PointerFmt(|f| write!(f, "pointer"));
+///
+/// assert_eq!(format!("{:p}", pointer), "pointer");
+/// ```
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Default)]
 pub struct PointerFmt<Fmt>(pub Fmt)
 where
@@ -230,6 +346,9 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> Pointer for PointerFmt<Fmt> {
 	}
 }
 
+/// ```rust
+/// let _: fn_formats::PointerFmt<_> = (|f: &mut core::fmt::Formatter| Ok(())).into();
+/// ```
 impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for PointerFmt<Fmt> {
 	fn from(fmt: Fmt) -> Self {
 		Self(fmt)
@@ -239,6 +358,16 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for PointerFmt<Fmt> {
 /// Implements [`UpperExp`] by calling the stored closure.
 ///
 /// [`UpperExp`]: https://doc.rust-lang.org/stable/core/fmt/trait.UpperExp.html
+///
+/// # Example
+///
+/// ```rust
+/// use fn_formats::UpperExpFmt;
+///
+/// let upper_exp = UpperExpFmt(|f| write!(f, "upper exp"));
+///
+/// assert_eq!(format!("{:E}", upper_exp), "upper exp");
+/// ```
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Default)]
 pub struct UpperExpFmt<Fmt>(pub Fmt)
 where
@@ -250,6 +379,9 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> UpperExp for UpperExpFmt<Fmt> {
 	}
 }
 
+/// ```rust
+/// let _: fn_formats::UpperExpFmt<_> = (|f: &mut core::fmt::Formatter| Ok(())).into();
+/// ```
 impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for UpperExpFmt<Fmt> {
 	fn from(fmt: Fmt) -> Self {
 		Self(fmt)
@@ -259,6 +391,16 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for UpperExpFmt<Fmt> {
 /// Implements [`UpperHex`] by calling the stored closure.
 ///
 /// [`UpperHex`]: https://doc.rust-lang.org/stable/core/fmt/trait.UpperHex.html
+///
+/// # Example
+///
+/// ```rust
+/// use fn_formats::UpperHexFmt;
+///
+/// let upper_hex = UpperHexFmt(|f| write!(f, "upperhex"));
+///
+/// assert_eq!(format!("{:X}", upper_hex), "upperhex");
+/// ```
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Default)]
 pub struct UpperHexFmt<Fmt>(pub Fmt)
 where
@@ -270,6 +412,9 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> UpperHex for UpperHexFmt<Fmt> {
 	}
 }
 
+/// ```rust
+/// let _: fn_formats::UpperHexFmt<_> = (|f: &mut core::fmt::Formatter| Ok(())).into();
+/// ```
 impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for UpperHexFmt<Fmt> {
 	fn from(fmt: Fmt) -> Self {
 		Self(fmt)
@@ -277,6 +422,24 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for UpperHexFmt<Fmt> {
 }
 
 /// Implements all format traits by calling the stored closure.
+///
+/// # Example
+///
+/// ```rust
+/// use fn_formats::ComprehensiveFmt;
+///
+/// let comprehensive = ComprehensiveFmt(|f| write!(f, "fmt"));
+///
+/// assert_eq!(format!("{:b}", comprehensive), "fmt");
+/// assert_eq!(format!("{:?}", comprehensive), "fmt");
+/// assert_eq!(format!("{}", comprehensive), "fmt");
+/// assert_eq!(format!("{:e}", comprehensive), "fmt");
+/// assert_eq!(format!("{:x}", comprehensive), "fmt");
+/// assert_eq!(format!("{:o}", comprehensive), "fmt");
+/// assert_eq!(format!("{:p}", comprehensive), "fmt");
+/// assert_eq!(format!("{:E}", comprehensive), "fmt");
+/// assert_eq!(format!("{:X}", comprehensive), "fmt");
+/// ```
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Default)]
 pub struct ComprehensiveFmt<Fmt>(pub Fmt)
 where
@@ -336,6 +499,9 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> UpperHex for ComprehensiveFmt<Fmt> 
 	}
 }
 
+/// ```rust
+/// let _: fn_formats::ComprehensiveFmt<_> = (|f: &mut core::fmt::Formatter| Ok(())).into();
+/// ```
 impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for ComprehensiveFmt<Fmt> {
 	fn from(fmt: Fmt) -> Self {
 		Self(fmt)
@@ -343,6 +509,34 @@ impl<Fmt: Fn(&mut Formatter) -> fmt::Result> From<Fmt> for ComprehensiveFmt<Fmt>
 }
 
 /// Implements all format traits by calling the respective stored closure.
+///
+/// # Example
+///
+/// ```rust
+/// use fn_formats::ComprehensiveFmtSeparate;
+///
+/// let comprehensive = ComprehensiveFmtSeparate {
+///     binary: |f| write!(f, "binary"),
+///     debug: |f| write!(f, "debug"),
+///     display: |f| write!(f, "display"),
+///     lower_exp: |f| write!(f, "lower exp"),
+///     lower_hex: |f| write!(f, "lower hex"),
+///     octal: |f| write!(f, "octal"),
+///     pointer: |f| write!(f, "pointer"),
+///     upper_exp: |f| write!(f, "upper exp"),
+///     upper_hex: |f| write!(f, "upper hex"),
+/// };
+///
+/// assert_eq!(format!("{:b}", comprehensive), "binary");
+/// assert_eq!(format!("{:?}", comprehensive), "debug");
+/// assert_eq!(format!("{}", comprehensive), "display");
+/// assert_eq!(format!("{:e}", comprehensive), "lower exp");
+/// assert_eq!(format!("{:x}", comprehensive), "lower hex");
+/// assert_eq!(format!("{:o}", comprehensive), "octal");
+/// assert_eq!(format!("{:p}", comprehensive), "pointer");
+/// assert_eq!(format!("{:E}", comprehensive), "upper exp");
+/// assert_eq!(format!("{:X}", comprehensive), "upper hex");
+/// ```
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Default)]
 pub struct ComprehensiveFmtSeparate<
 	BinaryFmt: Fn(&mut Formatter) -> fmt::Result,
