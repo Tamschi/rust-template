cd2e1a41675492a9738b68398552e1fa6d07b997
Tamme Schichler <tamme@schichler.dev>
2020-09-05 22:27:52 +0200

    Added more meta data

--- tests/version-numbers.rs
+++ tests/version-numbers.rs
@@ -1,3 +1,7 @@
+#[path = "constants_.rs"]
+mod constants;
+use constants::*;
+
 #[test]
 fn changelog() {
 	// This will become less useful with patches, so I'm on the lookout for a crate that lets me test major, minor and revision independently.
@@ -7,4 +11,23 @@ fn changelog() {
 #[test]
 fn html_root_url() {
 	version_sync::assert_html_root_url_updated!("src/lib.rs");
+}
+
+#[test]
+fn homepage() {
+	version_sync::assert_contains_regex!(
+		"Cargo.toml",
+		&format!(
+			r#"^homepage = "https://github\.com/{0}/{{name}}/tree/v{{version}}"$"#,
+			USER,
+		)
+	);
+}
+
+#[test]
+fn documentation() {
+	version_sync::assert_contains_regex!(
+		"Cargo.toml",
+		r#"^documentation = "https://docs\.rs/{name}/{version}"$"#
+	);
 }
