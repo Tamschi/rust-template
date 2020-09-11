ab5b48e8e636ebbe0aae662ae14a22ca155fb6a5
Tamme Schichler <tamme@schichler.dev>
2020-09-11 08:42:08 +0200

    Added default assignee and reviewer to Dependabot config

---
+++ tests/meta_dependabot.rs
@@ -0,0 +1,11 @@
+#[path = "constants_.rs"]
+mod constants;
+use constants::*;
+
+#[test]
+fn user_appears() {
+	version_sync::assert_contains_regex!(
+		".github/dependabot.yml",
+		&format!(r#"- "{user}"$"#, user = USER)
+	);
+}
